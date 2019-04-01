f9221b0939a2c14900a158394cd5e3a6ef9c2188
Zheyuan Xie <zyxie@seas.upenn.edu>
2019-04-01 14:10:37 -0400

    on_close terminate midi thread, add table view

--- sender/main.py
+++ sender/main.py
@@ -12,19 +12,21 @@ class MyApp(App):
     def onresize(self, emitter, w, h):
         # print(emitter, w, h)
         if int(w) > 450:
-            self.container.set_size(450, 150)
+            self.container.set_size(450, 540)
         else:
-            self.container.set_size(int(w), int(int(w)/3))
+            self.container.set_size(int(w), int(int(w)*(6.0/5.0)))
 
     def main(self):
-        container = gui.VBox(width='50%', height='20%')
+        container = gui.VBox(width='450px', height='540px')
 
         # Title
-        self.lbl = gui.Label('IPD 516 Band Controller')
+        self.lbl = gui.Label("Yim's Robotic Band")
+        self.lbl.style['font-size'] = '20px'
         container.append(self.lbl)
 
         # Serial Port Selection
         self.select_port_hbox = gui.HBox(width='100%')
+        self.select_port_hbox.style['font-size'] = '16px'
         self.select_port_label = gui.Label('Select Port:', width='40%')
         self.select_port_dropdown = gui.DropDown()
         self.select_port_refresh_btn = gui.Button(text='R', width='10%', height='80%')
@@ -38,6 +40,7 @@ class MyApp(App):
 
         # Music Selection
         self.select_music_hbox = gui.HBox(width='100%')
+        self.select_music_hbox.style['font-size'] = '16px'
         self.select_music_label = gui.Label('Select Music:', width='40%')
         self.select_music_dropdown = gui.DropDown()
         self.select_music_refresh_btn = gui.Button(text='R', width='10%', height='80%')
@@ -51,8 +54,9 @@ class MyApp(App):
         container.append(self.select_music_hbox)
 
         # Play/Stop Button
-        self.bt = gui.Button('Start', width = '50%', height = '20%')
+        self.bt = gui.Button('Start', width = '50%', height = '50px')
         self.bt.onclick.connect(self.btn_pressed)
+        self.bt.style['font-size'] = '25px'
         container.append(self.bt)
         self.line_contatiner = gui.Svg(width='100%',height='10px')
         self.line = gui.SvgLine(10,0,0,200)
@@ -64,6 +68,16 @@ class MyApp(App):
         container.append(self.info)
         self.progress = gui.Progress(_max=1000, width='100%')
         container.append(self.progress)
+        self.table = gui.TableWidget(17, 4, True, True, width='100%')
+        self.table.style['font-size'] = '14px'
+        self.table.style['text-align'] = 'center'   # TODO: text alignment
+        self.table.item_at(0,0).set_text("Channel")
+        self.table.item_at(0,1).set_text("Note")
+        self.table.item_at(0,2).set_text("Velocity")
+        self.table.item_at(0,3).set_text("Time")
+        for i in range(16):
+            self.table.item_at(i+1, 0).set_text("%d"%(i))
+        container.append(self.table)
         
         # MIDI Parser Thread
         self.mthread = None
@@ -79,7 +93,7 @@ class MyApp(App):
             self.mthread.join()
             self.mthread = None
             self.bt.set_text("Start")
-            self.info.set_text('-')
+            self.info.set_text('Stopped')
         else:
             self.mthread = threading.Thread(None, self.midi_thread, None)
             self.mthread_stop = False
@@ -89,6 +103,9 @@ class MyApp(App):
     
     def midi_thread(self):
         print("midi thread starting")
+        for i in range(16):
+            for j in range(3):
+                self.table.item_at(i+1,j+1).set_text("")
 
         try:
             ser_port = self.select_port_dropdown.get_value()
@@ -98,8 +115,8 @@ class MyApp(App):
             ser = None
 
         filename = self.select_music_dropdown.get_value()
-        self.info.set_text("Loading: "+filename)
-        mid = mido.MidiFile("song/"+filename)
+        self.info.set_text("Loading: " + filename)
+        mid = mido.MidiFile("song/" + filename)
         length = mid.length
         time = 0.0
         for msg in mid.play():
@@ -108,7 +125,10 @@ class MyApp(App):
                 return
             time = time + msg.time
             if (msg.type == "note_on"):
-                self.info.set_text("Playing: {Channel:%d, Note: %d, Time: %.2f}"%(msg.channel, msg.note, time))
+                self.info.set_text("Playing: " + filename) # {Channel:%d, Note: %d, Time: %.2f}"%(msg.channel, msg.note, time))
+                self.table.item_at(msg.channel+1,1).set_text("%d"%msg.note)
+                self.table.item_at(msg.channel+1,2).set_text("%d"%msg.velocity)
+                self.table.item_at(msg.channel+1,3).set_text("%.2f s"%time)
                 if ser is not None:
                     if (msg.channel == 2):
                         b = bytearray([0, msg.note, msg.velocity,ord('\n'),ord('\r')])
@@ -121,8 +141,16 @@ class MyApp(App):
                 self.progress.set_value(int(1000*(time/length)))
                 self.do_gui_update()
         self.bt.set_text("Start")
-        self.info.set_text('-')
+        self.info.set_text('Stopped')
         return
+    
+    def on_close(self):
+        print("closing...")
+        if self.mthread is not None:
+            self.mthread_stop = True
+            self.mthread.join()
+            print("force stopping midi thread")
+        return super().on_close()
 
 if __name__ == "__main__":
     start(MyApp)
