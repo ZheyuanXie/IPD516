157ed1c92209fe48d21ef22bffb911b76d003b6f
Zheyuan Xie <zyxie.sjtu@gmail.com>
2019-05-09 14:10:21 -0400

    ui update

--- sender/main.py
+++ sender/main.py
@@ -6,6 +6,16 @@ import os
 import threading
 from multiprocessing import Process, Pipe
 import ast
+import socket
+
+channel_map_table = {
+    "BEATLES_THE_-_Octopus_s_garden-mod.mid":"?",
+    "Foreplay_From_Long_Time-mhy.mid":"?",
+    "John Lee Hooker - Boom Boom Boom-final.mid":"0:0,1:1,8:2,9:3",
+    "Led_Zeppelin_-_Good_Times_Bad_Times mod.mid":"?",
+    "Scorpions_-_Rock_You_Like_a_Hurricane.mid":"?",
+    "FoolInTheRainOneString.mid":"1:0,2:1,4:3,10:9"
+}
 
 try:
     os.listdir("song")
@@ -15,12 +25,14 @@ except:
 
 
 def midi_process(filename, com_port, pipe, channel_map):
+    # print(channel_map)
     try:
         ser = serial.Serial(com_port, baudrate=115200)  # open serial port
     except Exception:
         print("Error Opening Serial.")
         ser = None
-    print("midi process starting")
+        return
+    # print("midi process starting")
 
     mid = mido.MidiFile(SONG_DIR + filename)
 
@@ -29,13 +41,12 @@ def midi_process(filename, com_port, pipe, channel_map):
     for msg in mid.play():
         time = time + msg.time
         if (msg.type == "note_on"):
-            if channel_map.get(msg.channel) is not None:
-                mapped_ch = channel_map[msg.channel]
+            if (msg.channel == 9):
+                ser.write(bytearray([msg.channel | 0x80, msg.note, msg.velocity]))
+            mapped_ch = channel_map.get(msg.channel)
+            if mapped_ch is not None:
                 pipe.send([mapped_ch, msg.note, msg.velocity, time, 1000*(time/length)])
-                if ser is not None:               
-                    b = bytearray([mapped_ch | 0x80, msg.note, msg.velocity])
-                    print("channel: %d, note: %d, vel: %d"%(mapped_ch, msg.note, msg.velocity))
-                    ser.write(b)
+                ser.write(bytearray([mapped_ch | 0x80, msg.note, msg.velocity]))
     return
 
 class MyApp(App):
@@ -45,30 +56,37 @@ class MyApp(App):
     def onresize(self, emitter, w, h):
         # print(emitter, w, h)
         if int(w) > 450:
-            self.container.set_size(450, 540)
+            self.container.set_size(450, 300)
         else:
-            self.container.set_size(int(w), int(int(w)*(6.0/5.0)))
+            self.container.set_size(int(w), int(int(w)*(2.0/3.0)))
 
     def main(self):
-        container = gui.VBox(width='450px', height='540px')
+        container = gui.VBox(width='450px', height='300px')
 
         # Title
         self.lbl = gui.Label("Prof. Yim's Robotic Band")
+        self.lblX = gui.Label("(X)")
+        self.lblX.onclick.connect(self.btn_exited)
         self.lbl.style['font-size'] = '20px'
-        container.append(self.lbl)
+        container.append(gui.HBox([self.lbl, self.lblX]))
 
         # Serial Port Selection
         self.select_port_hbox = gui.HBox(width='100%')
         self.select_port_hbox.style['font-size'] = '16px'
         self.select_port_label = gui.Label('Select Port:', width='40%')
         self.select_port_dropdown = gui.DropDown()
-        self.select_port_refresh_btn = gui.Button(text='R', width='10%', height='80%')
-        self.port_list = ['COM%s' % (i + 1) for i in range(256)]
+        # self.select_port_refresh_btn = gui.Button(text='R', width='10%', height='80%')
+        self.port_list = ['COM%s' % (i + 1) for i in range(16)]
         for port in self.port_list:
-            self.select_port_dropdown.append(port)
+            try:
+                ser = serial.Serial(port, baudrate=115200)  # open serial port
+                ser.close()
+                self.select_port_dropdown.append(port)
+            except Exception:
+                pass
         self.select_port_hbox.append(self.select_port_label)
         self.select_port_hbox.append(self.select_port_dropdown)
-        self.select_port_hbox.append(self.select_port_refresh_btn)
+        # self.select_port_hbox.append(self.select_port_refresh_btn)
         container.append(self.select_port_hbox)
 
         # Music Selection
@@ -76,15 +94,17 @@ class MyApp(App):
         self.select_music_hbox.style['font-size'] = '16px'
         self.select_music_label = gui.Label('Select Music:', width='40%')
         self.select_music_dropdown = gui.DropDown()
-        self.select_music_refresh_btn = gui.Button(text='R', width='10%', height='80%')
+        # self.select_music_refresh_btn = gui.Button(text='R', width='10%', height='80%')
         self.music_list = []
         for filename in os.listdir(SONG_DIR):
-            self.music_list.append(gui.DropDownItem(filename))
-            self.select_music_dropdown.append(self.music_list[-1])
+            if not os.path.isdir(SONG_DIR+filename):
+                self.music_list.append(gui.DropDownItem(filename))
+                self.select_music_dropdown.append(self.music_list[-1])
         self.select_music_hbox.append(self.select_music_label)
         self.select_music_hbox.append(self.select_music_dropdown)
-        self.select_music_hbox.append(self.select_music_refresh_btn)
+        # self.select_music_hbox.append(self.select_music_refresh_btn)
         container.append(self.select_music_hbox)
+        self.select_music_dropdown.onchange.connect(self.update_channel_assignment)
 
         # channel mapping
         channel_map_Hbox = gui.HBox(width='100%')
@@ -105,15 +125,19 @@ class MyApp(App):
         container.append(self.info)
         self.progress = gui.Progress(_max=1000, width='100%')
         container.append(self.progress)
-        self.table = gui.TableWidget(17, 4, True, True, width='100%')
+        self.table = gui.TableWidget(5, 4, True, True, width='100%')
         self.table.style['font-size'] = '14px'
         self.table.style['align-items'] = 'center'   # TODO: text alignment
         self.table.item_at(0,0).set_text("Channel")
         self.table.item_at(0,1).set_text("Note")
         self.table.item_at(0,2).set_text("Velocity")
         self.table.item_at(0,3).set_text("Time")
-        for i in range(16):
-            self.table.item_at(i+1, 0).set_text("%d"%(i))
+        # for i in range(4):
+        #     self.table.item_at(i+1, 0).set_text("%d"%(i))
+        self.table.item_at(1,0).set_text("CH0 - Piano")
+        self.table.item_at(2,0).set_text("CH1 - Bass")
+        self.table.item_at(3,0).set_text("CH2 - Guitar")
+        self.table.item_at(4,0).set_text("CH3 - Drum")
         container.append(self.table)
         
         # MIDI Parser Thread
@@ -124,17 +148,22 @@ class MyApp(App):
 
         # returning the root widget
         self.container = container
+        self.server_close = False
         return self.container
+
+    def btn_exited(self,widget):
+        self.server_close = True
+        self.close()
 
     def btn_pressed(self, widget):
         if self.mprocess is not None:
-            print("Stop")
+            # print("Stop")
             self.info.set_text("-")
             self.is_playing = False
             self.mthread.join()
             self.mthread = None
         else:
-            print("Start")
+            # print("Start")
             try:
                 ser = serial.Serial(self.select_port_dropdown.get_value(), baudrate=115200)  # open serial port
                 ser.close()
@@ -146,7 +175,6 @@ class MyApp(App):
             self.pipe = (tx, rx)
             self.is_playing = True
             channel_map = ast.literal_eval('{' +self.channel_map_textbox.get_value() + '}')
-            print(channel_map)
             self.mthread = threading.Thread(None, self.gui_thread, None)
             self.mprocess = Process(target = midi_process, \
                 args=(self.select_music_dropdown.get_value(), \
@@ -157,8 +185,11 @@ class MyApp(App):
             self.bt.set_text("Stop")
             self.info.set_text("Playing: "+self.select_music_dropdown.get_value())
     
+    def update_channel_assignment(self, widget, value):
+        self.channel_map_textbox.set_text(channel_map_table[value])
+    
     def gui_thread(self):
-        for i in range(16):
+        for i in range(4):
             for j in range(3):
                 self.table.item_at(i+1,j+1).set_text("")
         while(self.is_playing == True):
@@ -166,7 +197,7 @@ class MyApp(App):
                 recv = self.pipe[1].recv()
                 channel, note, velocity = recv[0], recv[1], recv[2]
                 time, progress = recv[3], recv[4]
-                if (velocity > 0):
+                if (velocity > 0) and (channel <= 3):
                     self.table.item_at(channel+1,1).set_text("%d"%note)
                     self.table.item_at(channel+1,2).set_text("%d"%velocity)
                     self.table.item_at(channel+1,3).set_text("%.2f s"%time)
@@ -182,11 +213,11 @@ class MyApp(App):
         self.bt.set_text("Start")
     
     def on_close(self):
-        print("closing...")
+        print("exiting...")
         if (self.is_playing):
             self.is_playing = False
             self.mthread.join()
-        return super().on_close()
+        super(MyApp, self).on_close()
 
 if __name__ == "__main__":
-    start(MyApp)
+    start(MyApp, standalone = True, width=450, height=300, resizable=False, title="MIDI Team")
--- sender/serial_test.py
+++ sender/serial_test.py
@@ -3,21 +3,23 @@ import threading
 import mido
 
 try:
-    ser = serial.Serial('COM5', baudrate=115200)  # open serial port
+    ser = serial.Serial('COM3', baudrate=115200)  # open serial port
 except Exception:
     print("Error Opening Serial.")
 
-mid = mido.MidiFile('song/John Lee Hooker - Boom Boom Boom.mid')
+mid = mido.MidiFile('sender/song/John Lee Hooker - Boom Boom Boom.mid')
 length = mid.length
 time = 0.0
 for msg in mid.play():
     time = time + msg.time
     if (msg.type == "note_on"): # control_change, program_change, note_on
-        if (msg.channel == 2):
-            b = bytearray([0 | 0x80, msg.note, msg.velocity])
+        if (msg.channel == 1):
+            b = bytearray([1 | 0x80, msg.note, msg.velocity])
             print("channel: %d, note: %d, vel: %d"%(msg.channel, msg.note, msg.velocity))
+            print('1',time.time())
             ser.write(b)
-        if (msg.channel == 9):
-            b = bytearray([msg.channel | 0x80, msg.note, msg.velocity])
+            print('2',time.time())
+        if (msg.channel == 0):
+            b = bytearray([0 | 0x80, msg.note, msg.velocity])
             print("channel: %d, note: %d, vel: %d"%(msg.channel, msg.note, msg.velocity))
             ser.write(b) 
